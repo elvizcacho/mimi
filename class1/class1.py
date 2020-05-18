0e8b2eaa7faad5079c392e3f9da37d67dfae1dca
Juan Vizcaino <elvizcacho@gmail.com>
2020-05-18 23:10:42 +0200

    Class1 + Homework

--- class1/class1.py
+++ class1/class1.py
@@ -1,4 +1,34 @@
-print "hola"
+import math
+
+var1 = "Text1" #String
+var2 = 2
+var3 = [var1, var2, "0", 0]
+var4 = [1, 2, 3, var2, var2, var3, var1, 8, 9, var1]
+var5 = 2
+
+if len(var4) < len(var3):
+    print("2 is greater than 1")
+    if var2 < len(var1):
+        print("HOla")
+    if var1 == "Text2":
+        print("HOla2")
+else:
+    print("2 is not greater than 1")
+    if var1 == "Text1":
+        print("HOlaX")
+print("4")
+
+
+# Take a ADN chain of 10 nucleotidos and count how many nucleotidos are in total and how many of each.
+# e.g. "gacctctatt"
+# If this ADN chain is given the program has to print:
+# Total number of nucleotidos: 10
+# "G: 1"
+# "A: 2"
+# "C: 3"
+# "T: 4"
+# You can only use ifs and what was seen in class
+
 
 
 
