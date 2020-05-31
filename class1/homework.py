4801a4b2ec50199577a9eb0b69f58aebef4f8d5c
mile <mimivizcainoc@gmail.com>
2020-05-31 15:14:24 +0200

    HOMEWORK

---
+++ class1/homework.py
@@ -0,0 +1,111 @@
+# I wen to Bioinformatic i choose this sequence homework: agttagagcg
+var1=["a","g","t","t","a","g","a","g","c","g"]
+varA= 0
+varG= 0
+varT= 0
+varC= 0
+print(len(var1))
+if var1[0] == "a":
+    varA=varA+1
+if var1[0] == "g":
+    varG=varG+1
+if var1[0] == "t":
+    varT=varT+1
+if var1[0] == "c":
+    varC=varC+1
+
+if var1[1] == "a":
+    varA=varA+1
+if var1[1] == "g":
+    varG=varG+1
+if var1[1] == "t":
+    varT=varT+1
+if var1[1] == "c":
+    varC=varC+1
+
+if var1[2] == "a":
+    varA=varA+1
+if var1[2] == "g":
+    varG=varG+1
+if var1[2] == "t":
+    varT=varT+1
+if var1[2] == "c":
+    varC=varC+1
+
+if var1[3] == "a":
+    varA=varA+1
+if var1[3] == "g":
+    varG=varG+1
+if var1[3] == "t":
+    varT=varT+1
+if var1[3] == "c":
+    varC=varC+1
+
+if var1[4] == "a":
+    varA=varA+1
+if var1[4] == "g":
+    varG=varG+1
+if var1[4] == "t":
+    varT=varT+1
+if var1[4] == "c":
+    varC=varC+1
+
+if var1[5] == "a":
+    varA=varA+1
+if var1[5] == "g":
+    varG=varG+1
+if var1[5] == "t":
+    varT=varT+1
+if var1[5] == "c":
+    varC=varC+1
+
+if var1[6] == "a":
+    varA=varA+1
+if var1[6] == "g":
+    varG=varG+1
+if var1[6] == "t":
+    varT=varT+1
+if var1[6] == "c":
+    varC=varC+1
+
+if var1[7] == "a":
+    varA=varA+1
+if var1[7] == "g":
+    varG=varG+1
+if var1[7] == "t":
+    varT=varT+1
+if var1[7] == "c":
+    varC=varC+1
+
+if var1[8] == "a":
+    varA=varA+1
+if var1[8] == "g":
+    varG=varG+1
+if var1[8] == "t":
+    varT=varT+1
+if var1[8] == "c":
+    varC=varC+1
+
+if var1[9] == "a":
+    varA=varA+1
+if var1[9] == "g":
+    varG=varG+1
+if var1[9] == "t":
+    varT=varT+1
+if var1[9] == "c":
+    varC=varC+1
+
+#answer
+print("A: " + str(varA))
+print("G: " + str(varG))
+print("T: " + str(varT))
+print("C: " + str(varC))
+
+
+
+
+
+
+
+
+
