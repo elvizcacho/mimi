7f0d2b906d725834616571e02213abc8a77f5df5
mile <mimivizcainoc@gmail.com>
2020-06-01 22:42:25 +0200

    solution

--- class1/homework.py
+++ class1/homework.py
@@ -1,99 +1,109 @@
 # I wen to Bioinformatic i choose this sequence homework: agttagagcg
-var1=["a","g","t","t","a","g","a","g","c","g"]
+sequence=[]
 varA= 0
 varG= 0
 varT= 0
 varC= 0
-print(len(var1))
-if var1[0] == "a":
+print(len(sequence))
+if len(sequence) >=1:
+    if sequence[0] == "a":
+        varA=varA+1
+    if sequence[0] == "g":
+        varG=varG+1
+    if sequence[0] == "t":
+        varT=varT+1
+    if sequence[0] == "c":
+        varC=varC+1
+
+if len(sequence) >=2:
+    if sequence[1] == "a":
+        varA=varA+1
+    if sequence[1] == "g":
+        varG=varG+1
+    if sequence[1] == "t":
+        varT=varT+1
+    if sequence[1] == "c":
+        varC=varC+1
+
+if len(sequence) >=3:
+ if sequence[2] == "a":
     varA=varA+1
-if var1[0] == "g":
+ if sequence[2] == "g":
     varG=varG+1
-if var1[0] == "t":
+ if sequence[2] == "t":
     varT=varT+1
-if var1[0] == "c":
+ if sequence[2] == "c":
     varC=varC+1
 
-if var1[1] == "a":
-    varA=varA+1
-if var1[1] == "g":
-    varG=varG+1
-if var1[1] == "t":
-    varT=varT+1
-if var1[1] == "c":
-    varC=varC+1
+if len(sequence) >=4:
+    if sequence[3] == "a":
+        varA=varA+1
+    if sequence[3] == "g":
+        varG=varG+1
+    if sequence[3] == "t":
+        varT=varT+1
+    if sequence[3] == "c":
+        varC=varC+1
 
-if var1[2] == "a":
-    varA=varA+1
-if var1[2] == "g":
-    varG=varG+1
-if var1[2] == "t":
-    varT=varT+1
-if var1[2] == "c":
-    varC=varC+1
+if len(sequence) >=5:
+    if sequence[4] == "a":
+        varA=varA+1
+    if sequence[4] == "g":
+        varG=varG+1
+    if sequence[4] == "t":
+        varT=varT+1
+    if sequence[4] == "c":
+        varC=varC+1
 
-if var1[3] == "a":
-    varA=varA+1
-if var1[3] == "g":
-    varG=varG+1
-if var1[3] == "t":
-    varT=varT+1
-if var1[3] == "c":
-    varC=varC+1
+if len(sequence) >=6:
+    if sequence[5] == "a":
+        varA=varA+1
+    if sequence[5] == "g":
+        varG=varG+1
+    if sequence[5] == "t":
+        varT=varT+1
+    if sequence[5] == "c":
+        varC=varC+1
 
-if var1[4] == "a":
-    varA=varA+1
-if var1[4] == "g":
-    varG=varG+1
-if var1[4] == "t":
-    varT=varT+1
-if var1[4] == "c":
-    varC=varC+1
+if len(sequence) >=7:
+    if sequence[6] == "a":
+        varA=varA+1
+    if sequence[6] == "g":
+        varG=varG+1
+    if sequence[6] == "t":
+        varT=varT+1
+    if sequence[6] == "c":
+        varC=varC+1
 
-if var1[5] == "a":
-    varA=varA+1
-if var1[5] == "g":
-    varG=varG+1
-if var1[5] == "t":
-    varT=varT+1
-if var1[5] == "c":
-    varC=varC+1
+if len(sequence) >=8:
+    if sequence[7] == "a":
+        varA=varA+1
+    if sequence[7] == "g":
+        varG=varG+1
+    if sequence[7] == "t":
+        varT=varT+1
+    if sequence[7] == "c":
+        varC=varC+1
 
-if var1[6] == "a":
-    varA=varA+1
-if var1[6] == "g":
-    varG=varG+1
-if var1[6] == "t":
-    varT=varT+1
-if var1[6] == "c":
-    varC=varC+1
+if len(sequence) >=9:
+    if sequence[8] == "a":
+        varA=varA+1
+    if sequence[8] == "g":
+        varG=varG+1
+    if sequence[8] == "t":
+        varT=varT+1
+    if sequence[8] == "c":
+        varC=varC+1
 
-if var1[7] == "a":
-    varA=varA+1
-if var1[7] == "g":
-    varG=varG+1
-if var1[7] == "t":
-    varT=varT+1
-if var1[7] == "c":
-    varC=varC+1
-
-if var1[8] == "a":
-    varA=varA+1
-if var1[8] == "g":
-    varG=varG+1
-if var1[8] == "t":
-    varT=varT+1
-if var1[8] == "c":
-    varC=varC+1
-
-if var1[9] == "a":
-    varA=varA+1
-if var1[9] == "g":
-    varG=varG+1
-if var1[9] == "t":
-    varT=varT+1
-if var1[9] == "c":
-    varC=varC+1
+if len(sequence) >=10:
+    if sequence[9] == "a":
+        varA=varA+1
+    if sequence[9] == "g":
+        varG=varG+1
+    if sequence[9] == "t":
+        varT=varT+1
+    if sequence[9] == "c":
+        varC=varC+1
 
 #answer
 print("A: " + str(varA))
